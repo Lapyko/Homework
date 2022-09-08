e565114f6de3d76bfddaf14163e29cdbd4b31aa4
Lapyko <113054648+Lapyko@users.noreply.github.com>
2022-09-08 19:17:10 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿string a = Console.ReadLine();
+    int c=5;
+string[] Psrt;
+Psrt=a.Split();
+for (int i = 0; i < Psrt.Length; i++)
+    if (Psrt[i].Length == c) Console.WriteLine(Psrt[i]);
+Console.ReadLine();
+
